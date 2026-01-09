b1bace1b19bad291faa099dec9c4956215c74a32
Uche Okereke <orezpablo@gmail.com>
2026-01-09 11:32:05 +0100

    using state

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
+import { useState } from "react";
+
+function Square() {
+  const [value, setValue] = useState("");
+  function handleClick() {
+    setValue("X");
+  }
+  return (
+    <button className="square" onClick={handleClick}>
+      {value}
+    </button>
+  );
+}
+
 export default function Board() {
   return (
     <>
       {Array.from({ length: 3 }).map((_, i) => (
         <div key={i} className="board-row">
           {Array.from({ length: 3 }).map((_, j) => (
-            <Square key={i * 3 + j} value={i * 3 + j + 1} />
+            <Square key={i * 3 + j} />
           ))}
         </div>
       ))}
     </>
-  );
-}
-
-function Square({ value }: { value: number }) {
-  function handleClick() {
-    console.log("clicked");
-  }
-  return (
-    <button className="square" onClick={handleClick}>
-      {value}
-    </button>
   );
 }
