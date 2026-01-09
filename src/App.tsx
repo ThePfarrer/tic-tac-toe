761a78f5a532d808efb541a62d4745bf77e7f92d
Uche Okereke <orezpablo@gmail.com>
2026-01-09 11:23:59 +0100

    component props

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ export default function Board() {
       {Array.from({ length: 3 }).map((_, i) => (
         <div key={i} className="board-row">
           {Array.from({ length: 3 }).map((_, j) => (
-            <button key={i * 3 + j} className="square">
-              {i * 3 + j + 1}
-            </button>
+            <Square key={i * 3 + j} value={i * 3 + j + 1} />
           ))}
         </div>
       ))}
     </>
   );
+}
+
+function Square({ value }: { value: number }) {
+  return <button className="square">{value}</button>;
 }
