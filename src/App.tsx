40a89aefacbd0d33c657f5c4e90ec4bdd5f88181
Uche Okereke <orezpablo@gmail.com>
2026-01-09 11:14:29 +0100

    convert square into board

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,15 @@
-export default function Square() {
-  return <button className="square">X</button>;
+export default function Board() {
+  return (
+    <>
+      {Array.from({ length: 3 }).map((_, i) => (
+        <div key={i} className="board-row">
+          {Array.from({ length: 3 }).map((_, j) => (
+            <button key={i * 3 + j} className="square">
+              {i * 3 + j + 1}
+            </button>
+          ))}
+        </div>
+      ))}
+    </>
+  );
 }
