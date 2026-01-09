f506f4bef5d792f8f4456db2412441a9b17cb7c3
Uche Okereke <orezpablo@gmail.com>
2026-01-09 12:03:54 +0100

    lifting state up

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
 import { useState } from "react";
 
-function Square() {
-  const [value, setValue] = useState("");
-  function handleClick() {
-    setValue("X");
-  }
+function Square({
+  value,
+  onSquareClick,
+}: {
+  value: string;
+  onSquareClick: () => void;
+}) {
   return (
-    <button className="square" onClick={handleClick}>
+    <button className="square" onClick={onSquareClick}>
       {value}
     </button>
   );
 }
 
 export default function Board() {
+  const [squares, setSquares] = useState<string[]>(Array(9).fill(""));
+
+  function handleClick(i: number) {
+    const nextSquares = squares.slice();
+    nextSquares[i] = "X";
+    setSquares(nextSquares);
+  }
+
   return (
     <>
       {Array.from({ length: 3 }).map((_, i) => (
         <div key={i} className="board-row">
           {Array.from({ length: 3 }).map((_, j) => (
-            <Square key={i * 3 + j} />
+            <Square
+              key={i * 3 + j}
+              value={squares[i * 3 + j]}
+              onSquareClick={() => handleClick(i * 3 + j)}
+            />
           ))}
         </div>
       ))}
